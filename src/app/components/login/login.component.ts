151627c2a2c409af2ef7ea27814fc7d8afcfbe87
Niketan Pingale <shalakapingale49@gmail.com>
2025-09-29 10:45:48 +0530

    add auth service for login and signup

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup,Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import ValidateForm from 'src/app/helpers/validateform';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -14,7 +16,7 @@ export class LoginComponent implements OnInit {
   eyeIcon:string = "fa-eye-slash";
 
   loginForm!:FormGroup;
-  constructor(private fb:FormBuilder) { }
+  constructor(private fb:FormBuilder,private auth:AuthService,private router:Router) { }
 
   ngOnInit(): void {
     this.loginForm =this.fb.group({
@@ -26,6 +28,20 @@ export class LoginComponent implements OnInit {
   onLogin(){
     if(this.loginForm.valid){
       console.log(this.loginForm.value);
+
+      //send the obj to  database
+      this.auth.login(this.loginForm.value)
+      .subscribe({
+        next:(res)=>{
+           alert(res.message);
+          console.log(res.message);
+          this.loginForm.reset();
+          this.router.navigate(['login']);
+        },
+        error:(err)=>{
+          alert(err?.error.message);
+        }
+      })
     }
     else{
       //throw the error using toaster and with required fields
--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import ValidateForm from 'src/app/helpers/validateform';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-signup',
@@ -13,7 +15,7 @@ export class SignupComponent implements OnInit {
   isText:boolean = false;
   eyeIcon:string = "fa-eye-slash";
   signupForm!:FormGroup;
-  constructor(private fb:FormBuilder) { }
+  constructor(private fb:FormBuilder,private auth:AuthService,private router:Router) { }
 
   ngOnInit(): void {
     this.signupForm = this.fb.group({
@@ -29,6 +31,19 @@ export class SignupComponent implements OnInit {
   onSignup(){
     if(this.signupForm.valid){
       console.log(this.signupForm.value);
+
+      //send the obj to database
+      this.auth.signup(this.signupForm.value)
+      .subscribe({
+        next:(res)=>{
+          alert(res.message);
+          this.signupForm.reset();
+          this.router.navigate(['login']);
+        },
+        error:(err)=>{
+          alert(err?.error.message);
+        }
+      })
     }
     else{
       //throw the error using toaster and with required fields
